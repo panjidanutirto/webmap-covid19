382303bc933c4382c9e165d189b49d4efda1e689
Panji Danutirto <51248448+panjidanutirto@users.noreply.github.com>
2021-01-18 07:51:09 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,20 +8,10 @@ $("#about-btn").click(function() {
   return false;
 });
 
-$(document).ready(function(){
-      var d = new Date();
-      $.ajax({url: "https://kawalcovid19.harippe.id/api/summary", success: function(result){
-           $("#kasus").html(result.confirmed.value);
-           $("#sembuh").html(result.recovered.value);
-           $("#perawatan").html(result.activeCare.value);
-           $("#meninggal").html(result.deaths.value);
-           $("#date").html(result.metadata.lastUpdatedAt);
-          }});
-      });
-
 function tampilkanCovid() {
-          $.getJSON('https://api.kawalcorona.com/indonesia/provinsi/', function (data) {
-             $.each(data, function (i, data) {
+          $.getJSON('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/ArcGIS/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=FID%3C100&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=', function (data) {
+             let features = data.features;
+             $.each(features, function (i, data) {
              $('#list-covid').append('<tr><td>'+ data.attributes.Provinsi +'</td><td class="text-center">'+ data.attributes.Kasus_Posi +'</td><td class="text-center">'+ data.attributes.Kasus_Semb +'</td><td class="text-center">'+ data.attributes.Kasus_Meni +'</td></tr>');
           });
         });
